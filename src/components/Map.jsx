0a58c292fce26f480b55c7c2f0db6b64950e79c3
Tijs Martens <tijs@bothrs.com>
2020-11-07 19:28:10 +0100

    get current location from the user

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,12 +1,32 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import ReactMapGl from "react-map-gl";
+import { Plugins } from "@capacitor/core";
 
 export default function Map() {
+  const { Geolocation } = Plugins;
+
   const [viewport, setViewport] = useState({
     longitude: 45,
     latitude: 45,
-    zoom: 10,
+    zoom: 18,
   });
+
+  useEffect(() => {
+    const getCurrentPosition = async () => {
+      const coordinates = await Geolocation.getCurrentPosition();
+      console.log("latitude", coordinates.coords.latitude);
+      console.log("longitude", coordinates.coords.longitude);
+
+      const oldVp = {...viewport}
+      oldVp.longitude = coordinates.coords.longitude
+      oldVp.latitude = coordinates.coords.latitude
+
+      setViewport({...oldVp})
+    };
+
+    getCurrentPosition()
+
+  }, [Geolocation]);
 
   return (
     <ReactMapGl
@@ -14,7 +34,7 @@ export default function Map() {
       height="100vh"
       {...viewport}
       mapboxApiAccessToken={process.env.REACT_APP_MAPBOX}
-      onViewportChange={vp => setViewport(vp)}
+      onViewportChange={(vp) => setViewport(vp)}
     >
       lines here
     </ReactMapGl>
