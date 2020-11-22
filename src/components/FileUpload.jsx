d326a07be86a0fcce80a72321d0573d4cb234684
Tijs Martens <tijs@bothrs.com>
2020-11-22 13:50:14 +0100

    send file to backend

---
+++ src/components/FileUpload.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+
+export default function FileUpload() {
+  const [file, setFile] = useState();
+
+  const selectFile = (event) => {
+    setFile(event.target.files[0]);
+  };
+
+  const uploadFile = () => {
+    const url = process.env.REACT_APP_BACKEND_ENDPOINT + "/uploadRoute";
+
+    const formData = new FormData();
+    formData.append("gpx", file);
+
+    fetch(url, {
+      method: "POST",
+      headers: {},
+      body: formData,
+    });
+  };
+
+  return (
+    <div>
+      <input
+        type="file"
+        name="gpxFile"
+        onChange={(event) => selectFile(event)}
+      />
+      {file && <button onClick={() => uploadFile()}>send to backend</button>}
+    </div>
+  );
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,25 @@
-import React from 'react'
+import React from "react";
+import { useHistory } from "react-router-dom";
+import styled from "styled-components";
 
+import FileUpload from "../components/FileUpload"
 
-export default function Home(){
-    return(
-        <div>
-            hi there, i'm the home page
-        </div>
-    )
+const Button = styled.button`
+  padding: 10px;
+`;
+
+const ButtonContainer = styled.div`
+  margin: auto;
+`;
+export default function Home() {
+  const history = useHistory();
+  return (
+    <div>
+      hi there, i'm the home page
+      <ButtonContainer>
+        <Button onClick={() => history.push("/map")}>go to nav</Button>
+      </ButtonContainer>
+      <FileUpload/>
+    </div>
+  );
 }
